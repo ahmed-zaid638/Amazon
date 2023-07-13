c20fb5b6533af4fe2fea736fe26d144645044c44
ahmed <ahmedzaida464@gmail.com>
2023-07-13 23:14:52 +0300

    test

--- app/cancel/page.jsx
+++ app/cancel/page.jsx
@@ -8,7 +8,7 @@ const Page = () => {
   return (
     <div className="w-full flex items-center justify-center h-[70vh] overflow-hidden flex-col">
       <div>
-        <h1 className="text-3xl font-bold">❌ Opps! Something Went Wrong.</h1>
+        <h1 className="text-3xl font-bold"> Opps! Something Went Wrong.</h1>
       </div>
       <Image src={"/gifs/cancel.gif"} alt="success image" width={500} height={500} priority={true} />
     </div>
